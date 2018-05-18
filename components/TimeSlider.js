2ef04528482bfda9ce0d2a962d507d763244cde1
Andy <KNOV Andrea>
2018-05-18 20:25:18 +0200

    time slider control buttons - play/pause, step back, step forward

--- components/TimeSlider.js
+++ components/TimeSlider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import { Button } from 'semantic-ui-react';
 
 class TimeSlider extends React.Component {
     constructor(props) {
@@ -8,28 +9,109 @@ class TimeSlider extends React.Component {
         let min = props.from.unix();
         let max = props.to.unix();
 
-        let step = props.frequency ? props.frequency : 1;
-
         this.state = {
             min: min,
             max: max,
-            step: step,
-            value: min
+            value: min,
+            isPlaying: false
         };
 
         this.onChange = this.onChange.bind(this);
+
+        this.togglePlay = this.togglePlay.bind(this);
+
+        this.moveStepBack = this.moveStepBack.bind(this);
+        this.moveStepForward = this.moveStepForward.bind(this);
     }
 
     onChange(event) {
-        var time = parseInt(event.target.value);
+        let time = parseInt(event.target.value);
         this.setState({
             value: time
         });
 
         if (this.props.callback) {
-            var timeDate = moment.unix(time);
+            let timeDate = moment.unix(time);
             this.props.callback(timeDate);
         }
+    }
+
+    togglePlay() {
+        if (this.timerId) {
+            this._clearTimer();
+        } else {
+            this.timerId = setInterval(
+                () => this.moveStepForward(),
+                1000
+            );
+        }
+
+        this.setState((prevState, props) => ({
+            isPlaying: !prevState.isPlaying
+        }));
+    }
+
+    _clearTimer() {
+        clearInterval(this.timerId);
+        this.timerId = null;
+    }
+
+    moveStepBack() {
+        this.setState((prevState, props) => {
+            let time = parseInt(prevState.value);
+            let step = parseInt(props.frequency || 1);
+            let newValue = time - step;
+
+            if (newValue < prevState.min) {
+                newValue = prevState.min;
+            }
+
+            if (time !== newValue) {
+                if (props.callback) {
+                    let timeDate = moment.unix(newValue);
+                    props.callback(timeDate);
+                }
+            }
+
+            return {
+                value: newValue
+            };
+        });
+    }
+
+    moveStepForward() {
+        this.setState((prevState, props) => {
+            let time = parseInt(prevState.value);
+            let step = parseInt(props.frequency || 1);
+            let newValue = time + step;
+
+            if (newValue > prevState.max) {
+                newValue = prevState.max;
+            }
+
+            if (this.timer && newValue === prevState.max) {
+                this._clearTimer();
+            }
+
+            if (time !== newValue) {
+                if (props.callback) {
+                    let timeDate = moment.unix(newValue);
+                    props.callback(timeDate);
+                }
+            }
+
+            return {
+                value: newValue
+            };
+        });
+    }
+
+    disableStepBackButton() {
+        //TODO handle disabling buttons
+    }
+
+    disableStepForwardButton() {
+        //TODO handle disabling buttons
     }
 
     valueToString(value) {
@@ -37,7 +119,17 @@ class TimeSlider extends React.Component {
     }
 
     render() {
+        const isPlaying = this.state.isPlaying;
+        const playPauseIcon = isPlaying ? 'pause' : 'play';
+
         return <div className="timeSlider">
+            <div className="controlButtons">
+
+                <Button icon={ playPauseIcon } onClick={ this.togglePlay }/>
+                <Button icon='step backward' onClick={ this.moveStepBack }/>
+                <Button icon='step forward' onClick={ this.moveStepForward }/>
+            </div>
+
             <div className="currentValue">
                 { moment.unix(this.state.value).format('L LT Z') }
             </div>
@@ -47,7 +139,7 @@ class TimeSlider extends React.Component {
                         className="slider"
                         min={ this.state.min }
                         max={ this.state.max }
-                        step={ this.state.step }
+                        step={ this.props.frequency }
                         value={ this.state.value }
                         onChange={ this.onChange }/>
             </div>
