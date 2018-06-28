84b37ccc41142020898a5f336aebe8b3a00b5dfa
Andy <KNOV Andrea>
2018-06-28 20:17:44 +0200

    fix - handle null value in phenomenon_time_to

--- components/DateRangeSelector.js
+++ components/DateRangeSelector.js
@@ -90,11 +90,15 @@ class DateRangeSelector extends React.Component {
     render() {
         let from = this._getCurrentValueString(this.state.fromDate, this.props.currentValues.from);
 
-        let lastPossibleMeasurement = this.state.toDate.clone().add(1, 'days').
-                subtract(this.props.currentValues.frequency, 'seconds');
-        let lastMeasurement = this.props.currentValues.to.clone().
-                subtract(this.props.currentValues.frequency, 'seconds');
-        let to = this._getCurrentValueString(lastPossibleMeasurement, lastMeasurement);
+        let to;
+        if (this.props.currentValues.to) {
+            let lastPossibleMeasurement = this.state.toDate.clone().add(1, 'days').
+            subtract(this.props.currentValues.frequency, 'seconds');
+            let lastMeasurement = this.props.currentValues.to.clone().
+            subtract(this.props.currentValues.frequency, 'seconds');
+            to = this._getCurrentValueString(lastPossibleMeasurement, lastMeasurement);
+        }
+
 
         return <div>
             <Form style={ formStyle }>
