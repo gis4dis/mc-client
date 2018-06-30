e1ba0a193b5d93c404f47fc4ea3f2eb0b6156f51
Andy <KNOV Andrea>
2018-06-30 19:42:01 +0200

    responsive design - show sidebar in bottom on mobile

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
         return (
             <html>
                 <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
                     <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css" />
                     <link rel="stylesheet" href="/static/css/react-datepicker.css" />
