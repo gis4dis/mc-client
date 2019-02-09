1c0ff3aa415639f91eb881c646cc6099cb9d936d
Andy <KNOV Andrea>
2019-02-09 19:18:13 +0100

    timeslider from and to property in state

--- components/TimeSlider.js
+++ components/TimeSlider.js
@@ -9,6 +9,8 @@ class TimeSlider extends React.Component {
         console.log('TimeSlider constructor - props.from: ', props.from);
         this.state = {
             value: props.from || 0,
+            from: props.from || 0,
+            to: props.to || 100,
             isPlaying: false
         };
 
@@ -28,7 +30,9 @@ class TimeSlider extends React.Component {
         if (nextProps.from !== this.props.from || nextProps.to !== this.props.to) {
             console.log('TimeSlider componentWillReceiveProps - from: ', nextProps.from);
             this.setState({
-                value: nextProps.from || 0
+                value: nextProps.from || 0,
+                from: nextProps.from,
+                to: nextProps.to
             });
         }
     }
@@ -198,8 +202,8 @@ class TimeSlider extends React.Component {
             <div className="sliderContainer">
                 <input type="range"
                         className="slider"
-                        min={ this.props.from || 0 }
-                        max={ this.props.to || 100 }
+                        min={ this.state.from || 0 }
+                        max={ this.state.to || 100 }
                         step={ this.props.frequency }
                         value={ this.state.value }
                         disabled={ this.props.disabled }
