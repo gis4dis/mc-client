0e0cfef9981b5aac18bb58da8ac0580bdbbdf671
Andy <KNOV Andrea>
2020-01-19 20:08:11 +0100

    present current time frame by its end, closes #73

--- components/timeControls/dateRangeSelector/DateRangeSelector.js
+++ components/timeControls/dateRangeSelector/DateRangeSelector.js
@@ -7,13 +7,8 @@ import DatePicker from 'react-datepicker';
 import { Button, Divider, Form, Icon, Label } from 'semantic-ui-react';
 
 import CalendarHeader from './CalendarHeader';
-import {
-    getEndOfPeriod,
-    getStartOfPeriod,
-    getLastPossibleObservationTime,
-    getLastObservationTime,
-} from '../../../utils/time';
 import { TIME_SLOTS } from '../../../appConfiguration';
+import { getObservationTime } from '../../../utils/time';
 
 const formStyle = {
     margin: '8px',
@@ -26,6 +21,12 @@ const presetButtonStyle = {
     width: 'calc(100% - 4px)',
 };
 
+const observationTimeLabelStyle = {
+    paddingLeft: '12px',
+    paddingRight: '8px',
+    cursor: 'default',
+};
+
 const isRangeValid = (from, to) => {
     return from.isSameOrBefore(to);
 };
@@ -34,33 +35,20 @@ const isSameDate = (value, currentValue) => {
     return value.isSame(currentValue, 'day');
 };
 
-const getCurrentValueString = (value, currentValue) => {
-    let result;
-    if (currentValue && !value.isSame(currentValue, 'second')) {
-        if (isSameDate(value, currentValue)) {
-            result = currentValue.format('LT');
-        } else {
-            result = currentValue.format('L LT');
-        }
-    }
-
-    return result;
-};
-
-const getShiftedRange = (state, shiftFn, timeZone) => {
+const getShiftedRange = (state, shiftFn) => {
     let diff;
     let from;
     let to;
 
     const { fromDate, toDate, maxDate } = state;
 
-    const fromMonthStart = getStartOfPeriod(fromDate.clone(), 'month', timeZone);
-    const toMonthEnd = getEndOfPeriod(fromDate.clone, 'month', timeZone);
+    const fromMonthStart = fromDate.clone().startOf('month');
+    const toMonthEnd = fromDate.clone().endOf('month');
 
     if (isSameDate(fromDate, fromMonthStart) && isSameDate(toDate, toMonthEnd)) {
         diff = toDate.diff(fromDate, 'month') + 1;
         from = shiftFn.call(fromDate, diff, 'month').startOf('month');
-        to = getEndOfPeriod(shiftFn.call(toDate, diff, 'month'), 'month', timeZone);
+        to = shiftFn.call(toDate, diff, 'month').endOf('month');
 
         if (maxDate.isSameOrBefore(to)) {
             to = maxDate.clone();
@@ -80,7 +68,9 @@ class DateRangeSelector extends React.Component {
     constructor(props) {
         super(props);
 
-        const maxDate = getEndOfPeriod(moment(), 'day', props.timeZone).subtract(1, 'days');
+        const maxDate = moment(`00${props.timeZone}`, 'HHZ')
+            .endOf('day')
+            .subtract(1, 'days');
 
         this.state = {
             fromDate: props.from,
@@ -155,7 +145,7 @@ class DateRangeSelector extends React.Component {
     }
 
     _notifyRangeFix() {
-        console.log("To date can't be before from date.");
+        console.warn("To date can't be before from date.");
         const { notifyUser } = this.props;
         notifyUser({
             text: "'To date' can't be before 'from date'.",
@@ -226,9 +216,15 @@ class DateRangeSelector extends React.Component {
         const { unit, subtract } = preset;
         const { callback, timeZone } = this.props;
 
-        const now = moment();
-        const from = getStartOfPeriod(now.clone(), unit, timeZone).subtract(subtract, `${unit}s`);
-        const to = getEndOfPeriod(now.clone(), unit, timeZone).subtract(subtract, `${unit}s`);
+        const now = moment(`00${timeZone}`, 'HHZ');
+        const from = now
+            .clone()
+            .startOf(unit)
+            .subtract(subtract, `${unit}s`);
+        const to = now
+            .clone()
+            .endOf(unit)
+            .subtract(subtract, `${unit}s`);
 
         if (callback) {
             callback(from, to);
@@ -244,145 +240,169 @@ class DateRangeSelector extends React.Component {
 
     render() {
         const { fromDate, toDate, maxDate, _nextDisabled } = this.state;
-        const { currentValues, loading, timeSlot, timeZone } = this.props;
+        const { currentValues, loading, timeSlot } = this.props;
         const { from: currentFrom, to: currentTo, valueDuration } = currentValues;
-        const from = getCurrentValueString(fromDate, currentFrom);
 
-        let to;
-        if (currentTo) {
-            const lastPossibleObservation = getLastPossibleObservationTime(
-                toDate,
-                valueDuration,
-                timeZone
-            );
-            const lastObservation = getLastObservationTime(currentTo, valueDuration, timeZone);
-            to = getCurrentValueString(lastPossibleObservation, lastObservation);
-        }
+        if (fromDate && toDate) {
+            const timeSlotConfig = TIME_SLOTS[timeSlot];
 
-        const timeSlotConfig = TIME_SLOTS[timeSlot];
+            let presetGroups;
+            if (timeSlotConfig) {
+                ({ presetGroups } = timeSlotConfig);
+            }
 
-        let presetGroups;
-        if (timeSlotConfig) {
-            ({ presetGroups } = timeSlotConfig);
-        }
+            let fromLabelColor = 'grey';
+            if (currentFrom && fromDate.isBefore(currentFrom)) {
+                fromLabelColor = 'red';
+            }
 
-        return (
-            <div className="main" style={{ position: 'relative' }}>
-                <Divider horizontal inverted style={{ marginTop: '18px' }}>
-                    Select date
-                </Divider>
+            let toLabelColor = 'grey';
+            if (
+                currentTo &&
+                currentTo.isBefore(
+                    toDate
+                        .clone()
+                        .add(1, 'days')
+                        .startOf('day')
+                )
+            ) {
+                toLabelColor = 'red';
+            }
 
-                <Form className="date-picker-wrapper column left">
-                    <Form.Field className="date-picker-field from">
-                        <div className="date-picker-input-labels-wrapper">
-                            <Label size="small" style={{ marginTop: '2px' }}>
-                                From date
-                            </Label>
-                            {from && !loading && (
-                                <Label basic color="red" pointing="left" size="small">
-                                    {from}
+            return (
+                <div className="main" style={{ position: 'relative' }}>
+                    <Divider horizontal inverted style={{ marginTop: '18px' }}>
+                        Select date
+                    </Divider>
+
+                    <Form className="date-picker-wrapper column left">
+                        <Form.Field className="date-picker-field from">
+                            <div className="date-picker-input-labels-wrapper">
+                                <Label size="small" style={{ marginTop: '2px' }}>
+                                    From date
                                 </Label>
-                            )}
-                        </div>
-                        <div className="date-picker-input-wrapper">
-                            <DatePicker
-                                selectsStart
-                                selected={fromDate}
-                                startDate={fromDate}
-                                endDate={toDate}
-                                maxDate={maxDate}
-                                renderCustomHeader={CalendarHeader}
-                                previousMonthButtonLabel=""
-                                nextMonthButtonLabel=""
-                                style={{ width: '105px' }}
-                                onChange={this.handleFromChange}
-                            />
-                        </div>
-                    </Form.Field>
+                                {currentFrom && !loading && (
+                                    <Label
+                                        tag
+                                        color={fromLabelColor}
+                                        size="small"
+                                        style={observationTimeLabelStyle}
+                                        title="End of the first observation"
+                                    >
+                                        {getObservationTime(currentFrom, valueDuration).format(
+                                            'L LT'
+                                        )}
+                                    </Label>
+                                )}
+                            </div>
+                            <div className="date-picker-input-wrapper">
+                                <DatePicker
+                                    selectsStart
+                                    selected={fromDate}
+                                    startDate={fromDate}
+                                    endDate={toDate}
+                                    maxDate={maxDate}
+                                    renderCustomHeader={CalendarHeader}
+                                    previousMonthButtonLabel=""
+                                    nextMonthButtonLabel=""
+                                    style={{ width: '105px' }}
+                                    onChange={this.handleFromChange}
+                                />
+                            </div>
+                        </Form.Field>
 
-                    <Form.Field className="date-picker-field to">
-                        <div className="date-picker-input-labels-wrapper">
-                            <Label size="small" style={{ marginTop: '2px' }}>
-                                To date
-                            </Label>
-                            {to && !loading && (
-                                <Label basic color="red" pointing="left" size="small">
-                                    {to}
+                        <Form.Field className="date-picker-field to">
+                            <div className="date-picker-input-labels-wrapper">
+                                <Label size="small" style={{ marginTop: '2px' }}>
+                                    To date
                                 </Label>
-                            )}
-                        </div>
-                        <div className="date-picker-input-wrapper">
-                            <DatePicker
-                                selectsEnd
-                                selected={toDate}
-                                startDate={fromDate}
-                                endDate={toDate}
-                                minDate={fromDate}
-                                maxDate={maxDate}
-                                renderCustomHeader={CalendarHeader}
-                                previousMonthButtonLabel=""
-                                nextMonthButtonLabel=""
-                                style={{ width: '105px' }}
-                                onChange={this.handleToChange}
-                            />
-                        </div>
-                    </Form.Field>
-                </Form>
+                                {currentTo && !loading && (
+                                    <Label
+                                        tag
+                                        color={toLabelColor}
+                                        size="small"
+                                        style={observationTimeLabelStyle}
+                                        title="End of the last observation"
+                                    >
+                                        {currentTo.format('L LT')}
+                                    </Label>
+                                )}
+                            </div>
+                            <div className="date-picker-input-wrapper">
+                                <DatePicker
+                                    selectsEnd
+                                    selected={toDate}
+                                    startDate={fromDate}
+                                    endDate={toDate}
+                                    minDate={fromDate}
+                                    maxDate={maxDate}
+                                    renderCustomHeader={CalendarHeader}
+                                    previousMonthButtonLabel=""
+                                    nextMonthButtonLabel=""
+                                    style={{ width: '105px' }}
+                                    onChange={this.handleToChange}
+                                />
+                            </div>
+                        </Form.Field>
+                    </Form>
 
-                <div className="column right">
-                    <Button.Group>
-                        <Button inverted icon color="teal" onClick={this._setPrevious}>
-                            <Icon name="angle left" />
-                        </Button>
-                        <Button
-                            inverted
-                            icon
-                            color="teal"
-                            disabled={_nextDisabled}
-                            onClick={this._setNext}
-                        >
-                            <Icon name="angle right" />
-                        </Button>
-                    </Button.Group>
+                    <div className="column right">
+                        <Button.Group>
+                            <Button inverted icon color="teal" onClick={this._setPrevious}>
+                                <Icon name="angle left" />
+                            </Button>
+                            <Button
+                                inverted
+                                icon
+                                color="teal"
+                                disabled={_nextDisabled}
+                                onClick={this._setNext}
+                            >
+                                <Icon name="angle right" />
+                            </Button>
+                        </Button.Group>
+                    </div>
+
+                    <Form style={formStyle}>
+                        {presetGroups &&
+                            presetGroups.map((group, index) => {
+                                const groupId = `group${index}`;
+                                return (
+                                    <div className="button-wrapper" key={groupId}>
+                                        {group.map(preset => {
+                                            return (
+                                                <Button
+                                                    className="preset-button"
+                                                    key={preset.title}
+                                                    inverted
+                                                    size="mini"
+                                                    color="teal"
+                                                    onClick={e => this._setPreset(preset)}
+                                                    style={presetButtonStyle}
+                                                >
+                                                    {preset.title}
+                                                </Button>
+                                            );
+                                        })}
+                                    </div>
+                                );
+                            })}
+                    </Form>
+
+                    <Divider horizontal inverted style={{ marginTop: '8px' }} />
+
+                    <style jsx>
+                        {`
+                            .button-wrapper {
+                                display: inline-block;
+                            }
+                        `}
+                    </style>
                 </div>
+            );
+        }
 
-                <Form style={formStyle}>
-                    {presetGroups &&
-                        presetGroups.map((group, index) => {
-                            const groupId = `group${index}`;
-                            return (
-                                <div className="button-wrapper" key={groupId}>
-                                    {group.map(preset => {
-                                        return (
-                                            <Button
-                                                className="preset-button"
-                                                key={preset.title}
-                                                inverted
-                                                size="mini"
-                                                color="teal"
-                                                onClick={e => this._setPreset(preset)}
-                                                style={presetButtonStyle}
-                                            >
-                                                {preset.title}
-                                            </Button>
-                                        );
-                                    })}
-                                </div>
-                            );
-                        })}
-                </Form>
-
-                <Divider horizontal inverted style={{ marginTop: '8px' }} />
-
-                <style jsx>
-                    {`
-                        .button-wrapper {
-                            display: inline-block;
-                        }
-                    `}
-                </style>
-            </div>
-        );
+        return '';
     }
 }
 
--- utils/time.js
+++ utils/time.js
@@ -1,59 +1,32 @@
-import moment from 'moment';
-
-/**
- * Returns new Moment instance time zone offset.
- * @param {Moment} date Moment instance to set time zone offset
- * @param {Number|String} timeZone time zone offset
- * @returns {Moment} new Moment instance representing date in given time zone
- */
-const getDateInTimeZone = (date, timeZone) => {
-    return moment(date).utcOffset(timeZone);
-};
-
-/**
- * Returns new Moment instance representing start of the given period in specified time zone.
- * @param {Moment} date Moment instance to find its start of the given period
- * @param {String} period time period
- * @param {Number|String} timeZone time zone offset
- * @returns {Moment} new Moment instance representing start of the given period
- */
-export const getStartOfPeriod = (date, period, timeZone) => {
-    return getDateInTimeZone(date, timeZone).startOf(period);
-};
-
-/**
- * Returns new Moment instance representing end of the given period in specified time zone.
- * @param {Moment} date Moment instance to find its end of the given period
- * @param {String} period time period
- * @param {Number|String} timeZone time zone offset
- * @returns {Moment} new Moment instance representing end of the given period
- */
-export const getEndOfPeriod = (date, period, timeZone) => {
-    return getDateInTimeZone(date, timeZone).endOf(period);
-};
-
-/**
- * Returns new Moment instance representing last possible observation time in specified time zone.
- * @param {Moment} date Moment instance representing date
- * @param {Number} frequency observation frequency in seconds
- * @param {Number|String} timeZone time zone offset
- * @returns {Moment} new Moment instance representing last possible observation
- */
-export const getLastPossibleObservationTime = (date, valueDuration, timeZone) => {
-    const dateClone = getDateInTimeZone(date, timeZone);
-    const startOfNextDay = dateClone.add(1, 'days').startOf('day');
-    return startOfNextDay.subtract(valueDuration, 'seconds');
-};
-
 /**
  * Returns new Moment instance representing last observation time in specified time zone.
  * @param {Moment} time Moment instance representing beginning of the last observation extended by observation frequency
- * @param {Number} frequency observation frequency in seconds
- * @param {Number|String} timeZone time zone offset
+ * @param {Number} valueDuration observation duration in seconds
  * @returns {Moment} new Moment instance representing last observation
  */
-export const getLastObservationTime = (time, valueDuration, timeZone) => {
-    const timeClone = moment(getDateInTimeZone(time, timeZone));
-    const lastTime = timeClone.subtract(valueDuration, 'seconds');
+export const getLastObservationTime = (time, valueDuration) => {
+    const lastTime = time.clone().subtract(valueDuration, 'seconds');
     return lastTime;
+};
+
+/**
+ * Returns new Moment instance representing time of observation.
+ * @param {Moment} time Moment instance representing beginning of the observation
+ * @param {Number} valueDuration observation duration in seconds
+ * @returns {Moment} new Moment instance representing end of observation
+ */
+export const getObservationTime = (time, valueDuration) => {
+    const endTime = time.clone().add(valueDuration, 'seconds');
+    return endTime;
+};
+
+/**
+ * Returns new Moment instance representing time of observation in UNIX seconds.
+ * @param {Moment} time Moment instance representing beginning of the observation
+ * @param {Number} valueDuration observation duration in seconds
+ * @returns {Number} new Moment instance representing end of observation
+ */
+export const getObservationTimeInSeconds = (time, valueDuration) => {
+    const endTime = time.unix() + valueDuration;
+    return endTime;
 };
